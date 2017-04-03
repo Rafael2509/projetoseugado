bb6624be960d2b1f94ffdef3f05a211f00c0a016
Rafael Souza <souza.rafaelpa@gmail.com>
2017-04-03 15:48:33 -0300

    Reshaping front home

--- js/global.js
+++ js/global.js
@@ -3,4 +3,37 @@ $(document).ready(function(){
 	$("input[type=text][name=birthday]").mask('99/99/9999');
 	$("input[type=text][name=start_date]").mask('99/99/9999');
 	$("input[type=text][name=finish_date]").mask('99/99/9999');
+	$("input[type=text][name=telphone]").mask('99/99/9999');
+	$cont = 0;
+	$verify = 0;
+	$('.employed input').each(function(){
+		if($(this).hasClass('required')){			
+			$cont ++;
+		}
+	});
+	$("#user input").on("blur", function(){ 
+		if($(this).hasClass('required')){
+			$verify ++;			
+		}
+		if($verify === $cont){
+			$('.locked:after').css("content","\f029");
+			$('.locked').removeClass('locked');
+		}
+	});
+	$("input[type=button][name=button-del]").click(function(){
+		$id = this.id;
+		$.ajax({
+		   url:   'global.php',
+		   type:  'POST',
+		   data:  $id,
+		   error: function() {
+		         alert('Erro ao tentar ação!');
+		   },
+		   success: function( texto ) { 
+		   		alert('ok');
+		   },
+		   beforeSend: function() {
+		   }
+		});
+	});
 });
